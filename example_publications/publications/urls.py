77a510a530e6c0b8ca360042e2b22aa0c86da021
jhon <jhonvi2@gmail.com>
2017-08-25 14:49:12 -0400

    update django 1.11

--- example_publications/publications/urls.py
+++ example_publications/publications/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import *
 from .views import PubPostListView, PubPostDetailView, TaggedListView, CategoryListView
 
-urlpatterns = patterns('',
-                       url(r'^$', PubPostListView.as_view(), name='post_list'),
-                       url(r'^(?P<slug>[-\w]+)/$', PubPostDetailView.as_view(), name='post_detail'),
-                       url(r'^tag/(?P<tag>[-\w]+)/$', TaggedListView.as_view(), name='posts_tagged'),
-                       url(r'^category/(?P<category>[-\w]+)/$', CategoryListView.as_view(), name='posts_category'),
-                       )
+urlpatterns = [
+    url(r'^$', PubPostListView.as_view(), name='post_list'),
+    url(r'^(?P<slug>[-\w]+)/$', PubPostDetailView.as_view(), name='post_detail'),
+    url(r'^tag/(?P<tag>[-\w]+)/$', TaggedListView.as_view(), name='posts_tagged'),
+    url(r'^category/(?P<category>[-\w]+)/$', CategoryListView.as_view(), name='posts_category'),
+]
